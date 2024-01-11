e27856d005dfa3a099641c22029fc74c8cd051dc
PaioniaNongsiej <126144173+PaioniaNongsiej@users.noreply.github.com>
2024-01-11 11:22:54 +0530

    Add files via upload

---
+++ src/main/java/org/nic/eVatReturns/EVatReturnsApplication.java
@@ -0,0 +1,13 @@
+package org.nic.eVatReturns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EVatReturnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EVatReturnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nic/eVatReturns/EVatReturnsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nic.eVatReturns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EVatReturnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
